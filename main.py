bfa10f828c642fcf2564ece3e84366fea836df3d
Roberto Londoño <roberthdrums@gmail.com>
2020-12-15 14:40:47 -0500

    axios-cors

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ from fastapi import HTTPException
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+    "http://localhost", "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
+
 @app.get('/')
 async def root():
     return {"mesagge": "Bienvenido a su Tienda Virtual"} 
